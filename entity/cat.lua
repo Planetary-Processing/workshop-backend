abe8ed6434ce8e9e7dea6b66ab521e2953131b53
Sam Sully <sam@planetaryprocessing.io>
2023-08-15 15:41:35 +0100

    basic game

---
+++ entity/cat.lua
@@ -0,0 +1,19 @@
+local function init(e)
+  e.Data.dir = -0.5
+end
+
+local function update(e, dt)
+  x, y, z = e:GetPosition()
+  if x < -40 then
+    e.Data.dir = 0.5
+  elseif x > 40 then
+    e.Data.dir = -0.5
+  end
+  e:Move(e.Data.dir, 0, 0)
+end
+
+local function meow(e, args)
+  print("meow")
+end
+
+return {init=init,update=update,rpcs={meow=meow}}
---
+++ init.lua
@@ -0,0 +1,5 @@
+function init()
+  if chunk.id == 1 then
+    api.createEntity("cat", 35, 25, 1, {name="jar jar"})
+  end
+end
