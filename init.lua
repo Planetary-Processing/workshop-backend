021d294fd429249633f7afcf3d140d7398f950d1
Sam Sully <sam@planetaryprocessing.io>
2023-10-05 15:07:06 +0100

    oops, tree spam
    Change-Id: I02d989cbc880728d765750ce7760364af0f09429

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@ function init()
   if chunk.id == 1 and not chunk.generated then
     api.createEntity("cat", 35, 25, 1, {name="jar jar"})
   end
-  if math.random() < 0.1 then
+  if math.random() < 0.1 and not chunk.generated then
     api.createEntity("tree", chunk.x*chunk.size + math.random()*chunk.size, chunk.y*chunk.size + math.random()*chunk.size, 1)
   end
 end
