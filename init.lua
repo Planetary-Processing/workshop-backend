214261b544e8bd151345494eabeddb015d81497e
Sam Sully <sam@planetaryprocessing.io>
2023-10-06 12:26:34 +0100

    another dumb bug
    Change-Id: Ib7e7fc55aa1dc307b6056a595cfd8ce49552749c

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
 function init()
   print(started)
   if chunk.id == 1 and not chunk.generated then
-    --for i=1,20 do
+    for i=1,1 do
       api.createEntity("cat", 35, 25, 1, {name="jar jar "..i})
-    --end
+    end
   end
   if math.random() < 0.1 and not chunk.generated then
     api.createEntity("tree", chunk.x*chunk.size + math.random()*chunk.size, chunk.y*chunk.size + math.random()*chunk.size, 1)
