230f2461df93669fdd4975fd32d8d68ad9ed86e1
Sam Sully <sam@planetaryprocessing.io>
2023-10-06 11:28:11 +0100

    many cats doing many things and moving around randomly
    Change-Id: I9fce13772ff385fa3105826a6c24617bb12b4e30

--- entity/cat.lua
+++ entity/cat.lua
@@ -1,16 +1,28 @@
 local function init(e)
-  e.Data.dir = -0.5
   e.Chunkloader = true
+  e.Data.target = {x=math.random(-100,100),y=math.random(-100,100)}
+  e.Data.stop = 0
 end
 
 local function update(e, dt)
-  x, y, z = e:GetPosition()
-  if x < -40 then
-    e.Data.dir = 0.5
-  elseif x > 40 then
-    e.Data.dir = -0.5
+  x, y, _ = e:GetPosition()
+  local dx = e.Data.target.x - x
+  local dy = e.Data.target.y - y
+  if e.Data.stop > 0 then
+    e.Data.stop = e.Data.stop - 1
+    return
   end
-  e:Move(e.Data.dir, 0, 0)
+  if dx < 2 and dy < 2 then
+    e.Data.stop = 5
+    e.Data.target = {x=math.random(-100,100),y=math.random(-100,100)}
+  else
+    if dx > dy then
+      dy = 0
+    elseif dy > dx then
+      dx = 0
+    end
+    e:Move(dx, dy, 0)
+  end
 end
 
 return {init=init,update=update}
