e0a79e37c9d00f8d45acd729a61a1335e740cd03
Sam Sully <sam@planetaryprocessing.io>
2023-10-06 11:44:39 +0100

    no stopping
    Change-Id: I217b47beb3728d2453457493d7ef4a11ff6d292e

--- entity/cat.lua
+++ entity/cat.lua
@@ -1,20 +1,14 @@
 local function init(e)
   e.Chunkloader = true
-  e.Data.target = {x=math.random(-100,100),y=math.random(-100,100)}
-  e.Data.stop = 0
+  e.Data.target = {x=math.random(100),y=math.random(100)}
 end
 
 local function update(e, dt)
   x, y, _ = e:GetPosition()
   local dx = e.Data.target.x - x
   local dy = e.Data.target.y - y
-  if e.Data.stop > 0 then
-    e.Data.stop = e.Data.stop - 1
-    return
-  end
   if dx < 2 and dy < 2 then
-    e.Data.stop = 5
-    e.Data.target = {x=math.random(-100,100),y=math.random(-100,100)}
+    e.Data.target = {x=math.random(500),y=math.random(500)}
   else
     if dx > dy then
       dx = 1
