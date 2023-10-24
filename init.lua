a017c39e838d0acb88961379a5f61f6a7c5221e4
Sam Sully <sam@planetaryprocessing.io>
2023-10-24 14:33:29 +0100

    .
    Change-Id: I7923336f820581173919bc69f5bcd1ab5ea539ff

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@ function init()
       api.createEntity("cat", 35, 25, 1, {name="jar jar "..i})
     end
   end
-  if math.random() < 0.1 and not chunk.generated then
+  if not chunk.generated then
     api.createEntity("tree", chunk.x*chunk.size + math.random()*chunk.size, chunk.y*chunk.size + math.random()*chunk.size, 1)
   end
 end
