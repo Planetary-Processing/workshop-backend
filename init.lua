5dd79b5e98f9d2fa7ec55290db87c098c7b66305
Sam Sully <sam@planetaryprocessing.io>
2023-11-11 13:24:03 +0000

    remove numbers
    Change-Id: Ib17707870e2ea0acf19dba978ebdea0c74834ad7

--- init.lua
+++ init.lua
@@ -2,11 +2,13 @@ function init()
   if chunk.X == 1 and chunk.Y == 1 and not chunk.Generated then
     for i=1,20 do
       api.CreateEntity("cat", -35, 25, 1, {name="jar jar "..i})
+    end
+    for i=1,5 do
       api.CreateEntity("dog", 35, 25, 1, {name="holly "..i})
     end
   end
   if not chunk.Generated then
-    for i=1,math.random(10) do
+    for i=1,math.random(4) do
       api.CreateEntity("tree", chunk.X*chunk.Size + math.random()*chunk.Size, chunk.Y*chunk.Size + math.random()*chunk.Size, 1, {})
     end
   end
