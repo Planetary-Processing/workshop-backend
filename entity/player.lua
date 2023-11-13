175763cfd01f6e7b0728b211f8b74b8229ce29c6
Sam Sully <sam@planetaryprocessing.io>
2023-11-13 22:59:37 +0000

    only move if from player
    Change-Id: Id2ad307240d9f8f16a94ac5050c149f972be51f1

--- entity/player.lua
+++ entity/player.lua
@@ -6,7 +6,7 @@ end
 
 local function message(e, msg)
   print(msg)
-  e:Move(msg.Data.x,msg.Data.y,0)
+  if msg.Client then e:Move(msg.Data.x,msg.Data.y,0) end
 end
 
 return {init=init,update=update,message=message}
