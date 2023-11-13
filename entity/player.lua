41b9c61521cbd9ae64e1514dec22a64d275eab4f
Sam Sully <sam@planetaryprocessing.io>
2023-11-13 22:47:35 +0000

    msg not e
    Change-Id: I354ce3dd8e0674b9537721a083c34f52f73c174f

--- entity/player.lua
+++ entity/player.lua
@@ -6,8 +6,7 @@ end
 
 local function message(e, msg)
   print(msg)
-  print(msg.Data)
-  e:Move(e.Data.x,e.Data.y,0)
+  e:Move(msg.Data.x,msg.Data.y,0)
 end
 
 return {init=init,update=update,message=message}
