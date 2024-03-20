f2afaa58eb873b1471eb31f4656e11dd322f8ff0
Sam Sully <sam@planetaryprocessing.io>
2024-03-19 19:00:38 -0700

    update template
    Change-Id: I22b89d66276f0ab7f07a6fbd7069206820f34787

--- entity/player.lua
+++ entity/player.lua
@@ -13,8 +13,15 @@ end
 local function message(e, msg)
   -- print and error output can be viewed per-chunk in the control panel
   print(msg)
-  -- if this is a client message (i.e. from a game client) then look in the message's Data field for x and y movement
-  if msg.Client then e:Move(msg.Data.x,msg.Data.y,0) end
+  -- if this is a client message (i.e. from a game client) then look in the message's Data table
+  if msg.Client then
+    local x, y, z = msg.Data.x, msg.Data.y, msg.Data.z
+    if msg.Data.threedee then
+      e:MoveTo(x,z,y) -- SwarmPlay uses 'y' for depth in 3 dimensional games, and 'z' for height
+    else
+      e:Move(x,y,0)
+    end
+  end
 end
 
 -- entity file must return table of this format
