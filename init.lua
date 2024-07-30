909478c951c05e1dadb2bdc0e3457f063f6868df
Sam Sully <sam@planetaryprocessing.io>
2024-07-31 00:53:12 +0100

    use new API

--- init.lua
+++ init.lua
@@ -3,10 +3,10 @@ function init()
   -- do this block of code if the chunk is at (0,0) in chunk space and has not been generated yet
   if chunk.X == 1 and chunk.Y == 1 and not chunk.Generated then
     for i=1,20 do
-      -- type, x, y, z, data
+      -- create entity with parameters: type, x, y, z, data
       -- type must match the name of one of the lua files in entity folder of this repo
       -- data is an arbitrary lua table
-      api.CreateEntity("cat", -35, 25, 0, {name="jar jar "..i})
+      api.entity.Create("cat", -35, 25, 0, {name="jar jar "..i})
     end
   end
   if not chunk.Generated then
@@ -14,7 +14,7 @@ function init()
       for j=1,chunk.Size,8 do
         if math.random() < 0.1 then
           -- create some tree entities at random locations, 'math' and other standard lua library functions (excluding io/os operations) are included
-          api.CreateEntity("tree", chunk.X*chunk.Size + i + math.random() - 0.5, chunk.Y*chunk.Size + j + math.random() -0.5, 0, {})
+          api.entity.Create("tree", chunk.X*chunk.Size + i + math.random() - 0.5, chunk.Y*chunk.Size + j + math.random() -0.5, 0, {})
         end
       end
     end
